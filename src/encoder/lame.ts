70c7e860beb91edd2ad9965d56bdf2ca82bec59e
Claire Wang <claire_wang123@yahoo.com>
2023-05-08 16:51:25 -0700

    update to work on netlify

--- src/encoder/lame.ts
+++ src/encoder/lame.ts
@@ -2,7 +2,7 @@ import { Encoder } from '../types';
 
 const vendorPrefix = process.env.IS_DEV
   ? '/vendor/'
-  : '/country-riddim/vendor/';
+  : '/vendor/';
 
 Object.assign(globalThis, {
   Mp3LameEncoderConfig: {
